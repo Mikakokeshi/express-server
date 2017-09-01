f281b577f11b64c6e2cb46f5fdfe7592ee0c031f
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-01 15:48:31 +0800

    ejs

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs')
 
+// middleware
+app.use(express.jason());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
   console.log('my custom middleware!')
   next();
 });
 
+app.get('/new', function(req, res){
+  res.render('new');
+});
+
+app.post('/create', function(req, res){
+  res.send(req.body.name);
+});
 
 app.get('/', function(req, res){
-  res.send('hello world')
+  res.render('index', {title: 'Welcome'});
 });
+
 
 app.get('/cats', function(req, res){
   res.send('meow meow')
@@ -25,5 +38,3 @@ app.get('/hello.txt', function(req, res) {
 app.listen(3000, function() {
   console.log('Listening on port 3000!');
 });
-
-ap
